c8e23deff6ba11a9603b7bf65dbe88d8ea53bab6
Eh1Ka6 <ehua@debian>
2014-05-27 21:37:24 +0200

    J'ai oublié le plus important ...

--- Search_Engine_2014/Code_Source.cpp
+++ Search_Engine_2014/Code_Source.cpp
@@ -26,7 +26,7 @@ Code_Sources::Code_Sources(std::string url)
             {
                  fclose(fp);
                  exec_parsing();
-
+                    // make it recursive
 
             }
 
@@ -41,25 +41,50 @@ Code_Sources::Code_Sources(std::string url)
 }
 void Code_Sources::exec_parsing()
 {
-    std::cout << "Lancement de l'algoritme de parsing " ;
+    std::cout << "Lancement de l'algoritme de parsing "<< std::endl ;
+     int status;
+     pid_t pID = fork();
+
+   if (pID == 0)                // child
+
+   {
     char *args[]= {"parsing.sript",spathTmp,(char *) 0};
     execv("parsing.script",args);
-    /*Creation de la liste d'url
-    typedef std::list<Url*> uList;
-    typedef std::list<Url*>::iterator uListIterator;
-    uList listUrl;
-    uListIterator itUrl;
-    strcat(spathTmp,".liste");
-
-
-    for( itUrl=listUrl.begin(); itUrl!=listUrl.end(); itUrl++ )
-    {
-        Url* pCurrentEnnemie = (*itor);
-        if( pCurrentEnnemie->Visible()==true ){ ... }
-
+    std::cout << "Child process ended normally" << std::endl;
+    exit(EXIT_SUCCESS);
     }
-    */
-    fpListe = fopen(spathTmp, "r");
+    else if (pID < 0)
+    {
+        std::cerr << "failed to fork" << std::endl ;
+    }
+    else
+    {
+       if ((pID = wait(&status)) == -1)
+       {
+            //Wait for child
+       }
+       else
+       {
+        strcat(spathTmp,".liste");
+        fpListe = fopen(spathTmp, "r");
+        listUrl.push_front (Url());
+            itUrl = listUrl.begin();
+        while (!feof(fpListe)  )
+        {
+
+            if (fgets ( (*itUrl).getname() , 500 , fpListe) != NULL );
+
+            listUrl.push_front (Url());
+            itUrl++;
+         }
+        }
+    }
+
+
+
+
+
+
 
 
 
@@ -67,11 +92,10 @@ void Code_Sources::exec_parsing()
 
     fclose(fpListe);
     //initalisation de la var liste
-
 }
 Code_Sources::~Code_Sources()
 {
 
-    curl_global_cleanup();
+  //  curl_global_cleanup();
 
 }
